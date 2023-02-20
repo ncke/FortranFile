327391d2e3ed82e6763414d11037b759c5e1cd1f
Nick Everitt <nicholaseveritt@gmail.com>
2023-02-20 22:06:33 +0000

    Move fortran error into a separate file

---
+++ Sources/FortranFile/Formats/FormatError.swift
@@ -0,0 +1,60 @@
+//
+//  FormatError.swift
+//  
+//
+//  Created by Nick on 20/02/2023.
+//
+
+import Foundation
+
+// MARK: - Format Error
+
+extension FortranFile {
+    
+    public struct FormatError: Error {
+        
+        public enum ErrorKind: CustomStringConvertible {
+            case missingDescriptor
+            case missingFieldDecimals
+            case missingFieldWidth
+            case unexpectedDimensions
+            case unexpectedSymbols
+        }
+        
+        public let kind: ErrorKind
+        public let input: String
+        public let offset: Int
+        public let length: Int
+    }
+    
+}
+
+extension FortranFile.FormatError.ErrorKind {
+    
+    public var description: String {
+        switch self {
+        case .missingDescriptor: return "Missing descriptor"
+        case .missingFieldDecimals: return "Missing field decimals"
+        case .missingFieldWidth: return "Missing field width"
+        case .unexpectedDimensions: return "Unexpected dimensions"
+        case .unexpectedSymbols: return "Unexpected symbols"
+        }
+    }
+    
+}
+
+extension FortranFile.FormatError: CustomStringConvertible {
+    
+    public var description: String {
+        let hats = String(repeating: " ", count: offset)
+        + String(repeating: "^", count: length)
+        
+        return """
+            Error parsing format: \
+            \(kind) in [\(offset)...\(offset + length - 1)]
+            \(input)
+            \(hats)
+            """
+    }
+    
+}
