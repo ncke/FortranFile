b4acdf3266dab47ad8a41238d8a0a7f5df28a255
Nick Everitt <nicholaseveritt@gmail.com>
2023-02-20 11:25:37 +0000

    Create package

---
+++ Sources/FortranFile/FortranFile.swift
@@ -0,0 +1,6 @@
+public struct FortranFile {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/FortranFileTests/FortranFileTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FortranFile
+
+final class FortranFileTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FortranFile().text, "Hello, World!")
+    }
+}
