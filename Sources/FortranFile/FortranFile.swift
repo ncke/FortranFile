8984562fc0ff1c56bde60176a2ddd83ab85e0ba1
Nick Everitt <nicholaseveritt@gmail.com>
2023-03-05 09:39:06 +0000

    Refactor read error to protocol

--- Sources/FortranFile/FortranFile.swift
+++ Sources/FortranFile/FortranFile.swift
@@ -54,4 +54,25 @@ extension FortranFile {
     
 }
 
+// MARK: - Read Error
 
+extension FortranFile {
+    
+    public struct ReadError: FortranFileError {
+        
+        public enum ErrorKind: String, CustomStringConvertible {
+            public var description: String { self.rawValue }
+            case internalError = "Internal error"
+            case expectedInteger = "Expected an integer value"
+            case expectedLogical = "Expected a logical value"
+            case expectedReal = "Expected a real value"
+            case unexpectedDecimalPoint = "Unexpected decimal point"
+        }
+        
+        public let kind: ErrorKind
+        public let input: String
+        public let offset: Int
+        public let length: Int
+    }
+    
+}
